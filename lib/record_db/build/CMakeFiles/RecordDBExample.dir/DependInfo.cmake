999a3b707d214326ac8c58605807d4fa6964706b
aaroneponymous <apaul9@u.rochester.edu>
2024-02-05 23:15:11 -0500

    Forgot to Change lib/record_db/CMakeLists.txt

--- lib/record_db/build/CMakeFiles/RecordDBExample.dir/DependInfo.cmake
+++ lib/record_db/build/CMakeFiles/RecordDBExample.dir/DependInfo.cmake
@@ -9,6 +9,7 @@ set(CMAKE_DEPENDS_LANGUAGES
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
   "/home/aaroneponymous/Aaron's Folder/University/Semesters/spring_2024/csc_263/project_1/lib/record_db/record_db.cpp" "CMakeFiles/RecordDBExample.dir/record_db.cpp.o" "gcc" "CMakeFiles/RecordDBExample.dir/record_db.cpp.o.d"
+  "/home/aaroneponymous/Aaron's Folder/University/Semesters/spring_2024/csc_263/project_1/lib/record_db/record_db_example.cpp" "CMakeFiles/RecordDBExample.dir/record_db_example.cpp.o" "gcc" "CMakeFiles/RecordDBExample.dir/record_db_example.cpp.o.d"
   )
 
 # Targets to which this target links.
--- lib/record_db/build/CMakeFiles/RecordDBExample.dir/cmake_clean.cmake
+++ lib/record_db/build/CMakeFiles/RecordDBExample.dir/cmake_clean.cmake
@@ -1,6 +1,8 @@
 file(REMOVE_RECURSE
   "CMakeFiles/RecordDBExample.dir/record_db.cpp.o"
   "CMakeFiles/RecordDBExample.dir/record_db.cpp.o.d"
+  "CMakeFiles/RecordDBExample.dir/record_db_example.cpp.o"
+  "CMakeFiles/RecordDBExample.dir/record_db_example.cpp.o.d"
   "RecordDBExample"
   "RecordDBExample.pdb"
 )
