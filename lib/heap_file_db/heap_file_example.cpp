3f35b9465dfd9d6098223b9d754482f66dead287
aaroneponymous <apaul9@u.rochester.edu>
2024-02-24 08:32:54 -0500

    Initialization Done: opening in write & read mode

--- lib/heap_file_db/heap_file_example.cpp
+++ lib/heap_file_db/heap_file_example.cpp
@@ -24,20 +24,20 @@ int main() {
     // write_fixed_len_pages(csv_file_name, output_file_name, 100000);
     // read_fixed_len_pages(output_file_name, 100000);
     Heapfile *test_heapfile = new Heapfile;
-    FILE *file = fopen("page_output", "a");
+    FILE *file = fopen("page_output", "w");
     if (file != nullptr)
     {
         std::cout << "Opened" << std::endl;
     }
 
-    init_heapfile(test_heapfile, 10000, file);
-    delete test_heapfile;
-
+    init_heapfile(test_heapfile, 500, file);
     fclose(file);
-
-
-
-
+    FILE *new_file = fopen("page_output", "r");
+    test_heapfile->file_ptr_ = new_file;
+    get_heapfile_directory(test_heapfile);
+    
+    fclose(new_file);
+    delete test_heapfile;
     return 0;
 }
 
